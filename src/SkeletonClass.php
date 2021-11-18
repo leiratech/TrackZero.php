6bd1ddd563340ec846602a9e2a57df952e6594b8
Adel Alzubeir <adelzu@outlook.com>
2021-11-18 19:21:35 +0300

    nothing to see

--- src/SkeletonClass.php
+++ src/SkeletonClass.php
@@ -4,25 +4,38 @@ declare(strict_types=1);
 
 namespace leira\trackzero;
 
-class SkeletonClass
+class TrackZeroClient
 {
-    /**
-     * Create a new Skeleton Instance
-     */
-    public function __construct()
-    {
-        // constructor body
-    }
+    public string $apiKey;
+    public string $baseUrl = "https://api.trackzero.io";
 
     /**
-     * Friendly welcome
-     *
-     * @param string $phrase Phrase to return
-     *
-     * @return string Returns the phrase passed in
+     * Create a new TrackZero Instance
      */
-    public function echoPhrase(string $phrase): string
+    public function __construct(string $apiKey)
     {
-        return $phrase;
+        $this->apiKey = $apiKey;
+    }
+
+    public function createAnalyticsSpace(string $analyticsSpaceId): bool
+    {
+        $ch = curl_init();
+        $queryParams = array("analyticsSpaceId"=>$analyticsSpaceId);
+        curl_setopt($ch, CURLOPT_URL,            $baseUrl . "/analyticsSpaces". '?' . http_build_query($queryParams) );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt($ch, CURLOPT_POST,           1 );
+        curl_setopt($ch, CURLOPT_POSTFIELDS,     "body goes here" ); 
+        curl_setopt($ch, CURLOPT_HTTPHEADER,     array("X-API-KEY"=>$this->apiKey)); 
+
+        $result=curl_exec ($ch);
+        echo($result);
+    }
+    public function echoPhrase(): string
+    {
+        return $this->apiKey2;
     }
 }
+
+
+$tz = new TrackZeroClient("Test Key");
+$tz->createAnalyticsSpace("test");
